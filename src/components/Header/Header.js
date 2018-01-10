fbdc9470e2caf8a8624906fe87f824a625be5a5c
Phill Oliver <phillolivercomp@gmail.com>
2018-01-10 21:16:18 +0000

    Added Header to project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import IconButton from "material-ui/IconButton";
+import MenuIcon from "material-ui-icons/Menu";
+
+import githubLogo from "../../assets/github.svg";
+
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="contrast" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography type="title" color="inherit">
+            Phillip Oliver
+          </Typography>
+          <IconButton color="contrast" aria-label="Menu" className="logo">
+            <img src={githubLogo} alt="logo" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;
