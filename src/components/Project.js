5c25c94f5f16f18a8f1dc47d9bb8921f503c774e
Phill Oliver <phillolivercomp@gmail.com>
2018-01-26 20:04:59 +0000

    Added displaying of project READMEs

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import { CircularProgress } from "material-ui/Progress";
 import Card, { CardContent, CardHeader } from "material-ui/Card";
 import Typography from "material-ui/Typography";
 import IconButton from "material-ui/IconButton";
+import ReactMarkdown from "react-markdown";
+
 import githubLogo from "../assets/github.svg";
 
+import { getReadMeUrl } from "../config";
 import { style } from "../styles/components/Project.style";
 
 class Project extends Component {
+  constructor() {
+    super();
+    this.state = {
+      readMeContent: null,
+      isLoading: true
+    };
+  }
+
+  componentDidMount = () => {
+    fetch(getReadMeUrl(this.props.prObject.name))
+      .then(res => res.text())
+      .then(out => {
+        this.setState({
+          readMeContent: out,
+          isLoading: false
+        });
+      });
+  };
+
   decameliseText = text => {
     return (
       text.charAt(0).toUpperCase() + text.replace(/([A-Z])/g, " $1").slice(1)
@@ -33,7 +56,22 @@ class Project extends Component {
             </IconButton>
           }
         />
-        <CardContent />
+        <Card>
+          <CardHeader
+            title={
+              <Typography type="subheading" component="i">
+                README.md
+              </Typography>
+            }
+          />
+          <CardContent>
+            {!this.state.isLoading ? (
+              <ReactMarkdown source={this.state.readMeContent} />
+            ) : (
+              <CircularProgress className="Progress" />
+            )}
+          </CardContent>
+        </Card>
       </Card>
     );
   }
--- src/config.js
+++ src/config.js
@@ -4,6 +4,14 @@ const getGithubURL = suffix => {
   return "https://api.github.com/users/" + githubUserName + (suffix || "");
 };
 
+const getReadMeUrl = project => {
+  return (
+    "https://raw.githubusercontent.com/PhillOliver-Dev/" +
+    project +
+    "/master/README.md"
+  );
+};
+
 const navBarLinks = {
   Home: {
     link: "/"
@@ -18,5 +26,6 @@ const navBarLinks = {
 
 module.exports = {
   getGithubURL,
+  getReadMeUrl,
   navBarLinks
 };
